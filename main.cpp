06e33cac534dcf58282049e1872c8cb3c1796fb0
tastyForReal <genuinelytasty@gmail.com>
2025-04-06 00:11:53 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,57 +1,87 @@
 #include "command_executor.hpp"
-#include "downloader.hpp"
-#include <cstdio>
-#include <fileapi.h>
 #include <filesystem>
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
-bool download_and_run(setup::dl_params params, std::vector<std::string> args = {})
+namespace fs = std::filesystem;
+
+fs::path get_downloads_dir()
 {
-    std::cout << "Downloading file from URL: " << params.url << std::endl;
+    return fs::path(std::getenv("USERPROFILE")).append("Downloads").string();
+}
 
-    setup::downloader dl(params);
-    auto result = dl.download(
-        [&]()
-        {
-            std::cout << "Download finished." << std::endl;
-            std::vector<std::string> command = {
-                (std::filesystem::path(params.output_dir) / std::filesystem::path(params.filename)).string()};
-            for(const auto& arg : args)
-            {
-                command.emplace_back(arg);
-            }
-            setup::execute_command(command);
-        });
+fs::path get_scoop_app_path(const std::string& app_name)
+{
+    auto path = fs::path(std::getenv("USERPROFILE")).append("scoop").append("apps").append(app_name).append("current");
 
-    if(result != setup::dl_result::SUCCESS)
+    if(fs::exists(path) && fs::is_directory(path))
     {
-        std::cerr << "Download failed: " << setup::downloader::get_result_message(result) << std::endl;
-        return false;
+        for(const auto& entry : fs::directory_iterator(path))
+        {
+            if(entry.is_directory() && entry.path().filename() == "bin")
+            {
+                path = entry.path();
+                break;
+            }
+        }
     }
 
-    return true;
+    return path;
+}
+
+fs::path get_scoop_file_path()
+{
+    return get_scoop_app_path("scoop").append("scoop.ps1");
 }
 
 int main()
 {
-    setup::execute_command({"powershell", "/c",
-        "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Verbose; Invoke-RestMethod -Uri "
-        "https://get.scoop.sh | Invoke-Expression"});
+    const std::string scoop_path = get_scoop_file_path().string();
 
-    setup::execute_command({"cmd", "/c", "scoop update *"});
-    setup::execute_command({"cmd", "/c", "scoop install ", "7zip", "adb", "cloc", "dotnet-sdk", "fastfetch", "ffmpeg",
-        "gh", "git", "imagemagick", "jq", "nodejs", "python"});
-    setup::execute_command({"cmd", "/c", "scoop cache rm *"});
-    setup::execute_command({"cmd", "/c", "scoop cleanup *"});
+    if(fs::exists(scoop_path) && fs::is_regular_file(scoop_path))
+    {
+        setup::execute_command({"powershell", "/c", scoop_path, "update", "*"});
+    }
+    else
+    {
+        setup::execute_command({"powershell", "/c",
+            "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -Verbose; Invoke-RestMethod "
+            "-Uri "
+            "https://get.scoop.sh -Verbose | Invoke-Expression -Verbose"});
+    }
 
-    download_and_run({.url = "https://vscode.download.prss.microsoft.com/dbazure/download/stable/"
-                             "4437686ffebaf200fa4a6e6e67f735f3edf24ada/VSCodeUserSetup-x64-1.99.0.exe",
-                         .output_dir = ".",
-                         .filename = "VSCodeUserSetup-x64-1.99.0.exe",
-                         .show_progress = true},
-        {"/SP-", "/SILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/mergetasks=!runcode"});
+    const std::vector<std::string> scoop_apps = {"7zip", "adb", "cloc", "curl", "dotnet-sdk", "fastfetch", "ffmpeg",
+        "gh", "git", "imagemagick", "jq", "msys2", "nodejs", "python"};
+
+    for(const auto& app : scoop_apps)
+    {
+        const auto app_path = get_scoop_app_path(app);
+        if(fs::exists(app_path) && fs::is_directory(app_path))
+        {
+            continue;
+        }
+
+        setup::execute_command({"powershell", "/c", scoop_path, "install", app});
+    }
+
+    setup::execute_command({"powershell", "/c", scoop_path, "cache", "rm", "*"});
+    setup::execute_command({"powershell", "/c", scoop_path, "cleanup", "*"});
+
+    const std::string curl_path = get_scoop_app_path("curl").append("curl.exe").string();
+    const std::string firefox_dl_path = get_downloads_dir().append("firefox_setup.exe").string();
+    const std::string vscode_dl_path = get_downloads_dir().append("vscode_setup.exe").string();
+
+    setup::execute_command({curl_path, "-L", "-o", firefox_dl_path,
+        "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"});
+    setup::execute_command({firefox_dl_path, "/S", "/PreventRebootRequired=true"});
+
+    setup::execute_command({curl_path, "-L", "-o", vscode_dl_path,
+        "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"});
+    setup::execute_command(
+        {vscode_dl_path, "/SP-", "/SILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/mergetasks=!runcode"});
+
+    fs::remove(firefox_dl_path);
+    fs::remove(vscode_dl_path);
 
     return 0;
 }
